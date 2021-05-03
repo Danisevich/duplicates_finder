529dc06c330a6a2bdd57e2aba81ff8ff6ae4baf3
EvgeniyDanisevich <danisevich.e@gmail.com>
2021-05-03 17:37:46 +0300

    test3

--- src/main/java/ru/danisevich/Application.java
+++ src/main/java/ru/danisevich/Application.java
@@ -2,6 +2,6 @@ package ru.danisevich;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Application");
+        System.out.println("Not application");
     }
 }
